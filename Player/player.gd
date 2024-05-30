112731384f908bf8b8212c0376ff16e1f7cb8b0c
YuPengtao <ypt@this0.com>
2024-05-31 04:06:28 +0800

    feat: part4.奔跑、休息这8个动作的动画制作、播放

--- Player/player.gd
+++ Player/player.gd
@@ -6,6 +6,8 @@ const FRICTION = 10 * 60
 
 var input_vector = Vector2.ZERO
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 func _physics_process(delta: float) -> void:
 	
 	# 更新输入向量（仅当输入发生变化时才重新计算归一化）
@@ -16,8 +18,14 @@ func _physics_process(delta: float) -> void:
 	
 	#非零向量
 	if input_vector != Vector2.ZERO:
+		if input_vector.x >0:
+			animation_player.play("RunRight")
+		else :
+			animation_player.play("RunLeft")
+			
 		velocity = velocity.move_toward(input_vector * MAX_SPEED,ACCELERATION * delta - FRICTION * delta)
 	else :
+		animation_player.play("IdleRight")
 		velocity = velocity.move_toward(Vector2.ZERO,FRICTION * delta)	#摩擦力
 	
 	move_and_slide()
