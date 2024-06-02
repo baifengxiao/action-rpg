a8ce57084b8a7c00d5d8e34a7b1287112ba932c3
YuPengtao <ypt@this0.com>
2024-06-02 18:58:25 +0800

    feat: part8.为角色添加攻击动作

--- Player/player.gd
+++ Player/player.gd
@@ -4,14 +4,32 @@ const ACCELERATION = 15 * 60
 const MAX_SPEED = 100
 const FRICTION = 10 * 60
 
+enum {
+	MOVE,
+	ATTACK
+}
+
+var state = MOVE
+
 var input_vector = Vector2.ZERO
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var animationState =animation_tree.get("parameters/playback")
 
+func _ready() -> void:
+	animation_tree.active=true
+
 func _physics_process(delta: float) -> void:
 	
+	match state:
+		MOVE:
+			move_state(delta)
+		
+		ATTACK:
+			attack_state(delta)
+		
+func move_state(delta):
 	# 更新输入向量（仅当输入发生变化时才重新计算归一化）
 	input_vector.x=Input.get_axis("ui_left", "ui_right")
 	input_vector.y=Input.get_axis("ui_up", "ui_down")
@@ -27,6 +45,7 @@ func _physics_process(delta: float) -> void:
 			#animation_player.play("RunLeft")
 		animation_tree.set("parameters/Idle/blend_position",input_vector)
 		animation_tree.set("parameters/Run/blend_position",input_vector)
+		animation_tree.set("parameters/Attack/blend_position",input_vector)
 		animationState.travel("Run")
 		
 		velocity = velocity.move_toward(input_vector * MAX_SPEED,ACCELERATION * delta - FRICTION * delta)
@@ -36,4 +55,13 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity.move_toward(Vector2.ZERO,FRICTION * delta)	#摩擦力
 	
 	move_and_slide()
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
 
+func attack_state(delta):
+	# 清除记忆的速度
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+
+func attack_animation_finished():
+	state = MOVE
