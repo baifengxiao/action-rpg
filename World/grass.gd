44860dc0c9c06c55a8f69f08b6607b82db80e5c6
YuPengtao <ypt@this0.com>
2024-06-04 07:23:05 +0800

    feat: part10.伤害检测，物理层优化

--- World/grass.gd
+++ World/grass.gd
@@ -1,17 +1,19 @@
 extends Node2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("attack"):
-		
-		#引用摧毁特效
-		const GRASS_EFFECT = preload("res://Effects/grass_effect.tscn")
-		#实例化
-		var grassEffect = GRASS_EFFECT.instantiate()
-		
-		#用代码实例化它，要求它必须是一个节点的子节点，这里是world节点的子节点
-		var world = get_tree().current_scene
-		world.add_child(grassEffect)
-		
-		grassEffect.global_position = global_position
-		queue_free()
+func create_grass_effect():	
+	const GRASS_EFFECT = preload("res://Effects/grass_effect.tscn")
+	var grassEffect = GRASS_EFFECT.instantiate()
+	var world = get_tree().current_scene
+	world.add_child(grassEffect)
+	grassEffect.global_position = global_position
+
+#func _process(delta: float) -> void:
+	#if Input.is_action_just_pressed("attack"):
+		#
+		#
+		#queue_free()
+
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	create_grass_effect()
+	queue_free()
